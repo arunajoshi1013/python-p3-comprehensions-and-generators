ba00e8ec933c382cff44725b9bf47b1be86b99a3
Aruna  Joshi <arunajoshi1013@gmail.com>
2023-07-09 20:53:42 -0700

    Fix return evens

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -6,7 +6,7 @@ def even_num(even_list, num):
 def return_evens(num_list):
     even_list = []
     [even_num(even_list, num) for num in num_list]
-    print(even_list)
+    return even_list
 
 def make_exclamation(sentence_list):
     return [word + "!" for word in sentence_list]
